8df3d305ccc21625359613342a309fb86200906f
조현일 <himzei@gmail.com>
2022-08-22 21:52:32 +0900

    mouse wheel css

--- custom.js
+++ custom.js
@@ -9,4 +9,12 @@ $(function () {
     speed: 1000,
     pauseOnHover: false,
   });
+
+  // Type It
+  $("#typing").typeIt({
+    strings: ["Business strategy.", "Innovation Plan", "Creative Idea"],
+    speed: 100,
+    autoStart: true,
+    breakLines: false,
+  });
 });
