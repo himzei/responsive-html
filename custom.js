2582da335f75bcdb8df426a7fcad333cae2f1493
조현일 <himzei@gmail.com>
2022-08-24 07:58:27 +0900

    slick review

--- custom.js
+++ custom.js
@@ -10,6 +10,14 @@ $(function () {
     pauseOnHover: false,
   });
 
+  // slick - Review
+  $(".review-slider").slick({
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
   // Type It
   $("#typing").typeIt({
     strings: ["Business strategy.", "Innovation Plan", "Creative Idea"],
