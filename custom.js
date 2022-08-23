1693ce5601d2686b950fe4e4700be21ea22c7be7
조현일 <himzei@gmail.com>
2022-08-24 07:42:30 +0900

    faq jquery

--- custom.js
+++ custom.js
@@ -18,3 +18,24 @@ $(function () {
     breakLines: false,
   });
 });
+
+// FAQ Accordion
+$(function () {
+  // 처음시작하는 아코디언은 펼쳐라
+  $(".faq-desc").eq(0).show();
+
+  $(".faq-title").click(function () {
+    // faq-title 클릭했을 때 다음에 있는 desc를 슬라이드 다운
+    $(this).next().stop().slideDown();
+
+    // faq-title 의 부모(parent) 형제(siblings[faq-item]) 그리고 아들(children)을 슬라이드 업
+    $(this)
+      .parent()
+      .siblings(".faq-item")
+      .children(".faq-desc")
+      .stop()
+      .slideUp();
+    $(this).parent().addClass("active");
+    $(this).parent().siblings().removeClass("active");
+  });
+});
